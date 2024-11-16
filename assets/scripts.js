e27b781f3bd03321457c4177ec6ae3bcc2511500
Christopher <harper.christopher91@gmail.com>
2024-11-16 17:43:31 -0500

    Added Special Offer promo. Modal linking to jotform for client info capture. Sytles and scripts updated. Mobile tested.

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,27 @@
-const agreeCheckbox = document.querySelector("#agree");
-const agreeButton = document.querySelector("#agreeButton");
+// const agreeCheckbox = document.querySelector("#agree");
+// const agreeButton = document.querySelector("#agreeButton");
 
-agreeButton.href = "";
-agreeButton.innerHTML = "Cancel";
+// agreeButton.href = "";
+// agreeButton.innerHTML = "Cancel";
 
-agreeCheckbox.addEventListener("change", () => {
-    if (agreeCheckbox.checked) {
-        agreeButton.href = "https://squareup.com/appointments/book/ujdxbo54uafruw/L4NWXG0218M8G/start";
-        agreeButton.innerHTML = "Book Now";
-    } else {
-        agreeButton.href = "";
-        agreeButton.innerHTML = "Cancel";
+// agreeCheckbox.addEventListener("change", () => {
+//     if (agreeCheckbox.checked) {
+//         agreeButton.href = "https://squareup.com/appointments/book/ujdxbo54uafruw/L4NWXG0218M8G/start";
+//         agreeButton.innerHTML = "Book Now";
+//     } else {
+//         agreeButton.href = "";
+//         agreeButton.innerHTML = "Cancel";
+//     }
+//   });
+
+var count = 0;
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() >= $(
+        document).offset().top + $(document).
+            outerHeight() - window.innerHeight && count <= 0 ) {
+
+         $("#noticeModal").modal('show');
+         count++;
     }
-  });
+}); 
