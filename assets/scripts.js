248d4135697518b406183c73963d5cc1f10d779b
Kit <harper.christopher91@gmail.com>
2026-04-18 19:56:37 -0400

    Adding cookie consent banner. responsive

--- assets/scripts.js
+++ assets/scripts.js
@@ -25,3 +25,53 @@ $(window).on('scroll', function () {
          count++;
     }
 }); 
+
+
+function hideBanner() {
+    document.getElementById('cookie-consent-banner').style.display = 'none';
+}
+
+if (localStorage.getItem('consentMode') === null) {
+
+    document.getElementById('btn-accept-all').addEventListener('click', function() {
+        setConsent({
+            necessary: true,
+            analytics: true
+        });
+        hideBanner();
+    });
+    document.getElementById('btn-accept-some').addEventListener('click', function() {
+        setConsent({
+            necessary: true,
+            analytics: document.getElementById('consent-analytics').checked
+        });
+        hideBanner();
+    });
+    document.getElementById('btn-reject-all').addEventListener('click', function() {
+        setConsent({
+            necessary: false,
+            analytics: false
+        });
+        hideBanner();
+    });
+    document.getElementById('btn-hide-banner').addEventListener('click', function() {
+        hideBanner();
+    });
+    document.getElementById('cookie-consent-banner').style.display = 'block';
+}
+
+else {
+    hideBanner();
+}
+
+function setConsent(consent) {
+    const consentMode = {
+        'functionality_storage': consent.necessary ? 'granted' : 'denied',
+        'security_storage': consent.necessary ? 'granted' : 'denied',
+        'analytics_storage': consent.analytics ? 'granted' : 'denied',
+        
+    };
+    gtag('consent', 'update', consentMode);
+    localStorage.setItem('consentMode', JSON.stringify(consentMode));
+}
+      
